7a18f64a41227e8b4d277a4b413017837b23622a
dmerci1 <danmercia@gmail.com>
2018-05-29 19:14:49 -0400

    added second browser window for settings

---
+++ src/components/TrayWindow.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TrayWindow extends Component {
+  render() {
+    return (
+      <h3>Tray Window</h3>
+    )
+  }
+}
+
+export default TrayWindow;
--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return (<div>
-      <h2>Welcome to React!</h2>
-    </div>);
+      <h2>Mirror Assistant</h2>
+    </div>);  
   }
 }
